f5ad6cfb0ae3347bb297f8f7e02675d45021f96b
Serhii Chernenko <serhii.chernenko@vaimo.com>
2023-10-15 17:20:10 +0300

    link to YT channel added

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.APP_PORT ?? 3000
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send(`Subscribe to <a href="https://www.youtube.com/channel/@serhii.chernenko">my channel</a>.`)
 })
 
 app.listen(PORT, () => {
